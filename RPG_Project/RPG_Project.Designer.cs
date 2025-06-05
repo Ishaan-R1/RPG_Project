a069586a7084fb0f4bd18a5ece2d5838ca9f7c25
Ishaan <ishaanrahim@gmail.com>
2025-06-05 12:24:33 +0100

    Creating Player class

--- RPG_Project/RPG_Project.Designer.cs
+++ RPG_Project/RPG_Project.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            lblHitPoints = new Label();
+            lblGold = new Label();
+            lblExperience = new Label();
+            label5 = new Label();
+            lblLevel = new Label();
+            btnTest = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Hit Points:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 46);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Gold:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(18, 74);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Experience:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(18, 100);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Level:";
+            // 
+            // lblHitPoints
+            // 
+            lblHitPoints.AutoSize = true;
+            lblHitPoints.Location = new Point(110, 19);
+            lblHitPoints.Name = "lblHitPoints";
+            lblHitPoints.Size = new Size(0, 20);
+            lblHitPoints.TabIndex = 4;
+            // 
+            // lblGold
+            // 
+            lblGold.AutoSize = true;
+            lblGold.Location = new Point(110, 46);
+            lblGold.Name = "lblGold";
+            lblGold.Size = new Size(0, 20);
+            lblGold.TabIndex = 5;
+            // 
+            // lblExperience
+            // 
+            lblExperience.AutoSize = true;
+            lblExperience.Location = new Point(110, 74);
+            lblExperience.Name = "lblExperience";
+            lblExperience.Size = new Size(0, 20);
+            lblExperience.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(110, 100);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 20);
+            label5.TabIndex = 7;
+            // 
+            // lblLevel
+            // 
+            lblLevel.AutoSize = true;
+            lblLevel.Location = new Point(110, 100);
+            lblLevel.Name = "lblLevel";
+            lblLevel.Size = new Size(0, 20);
+            lblLevel.TabIndex = 8;
+            // 
+            // btnTest
+            // 
+            btnTest.Location = new Point(161, 176);
+            btnTest.Name = "btnTest";
+            btnTest.Size = new Size(94, 29);
+            btnTest.TabIndex = 9;
+            btnTest.Text = "Test";
+            btnTest.UseVisualStyleBackColor = true;
+            btnTest.Click += btnTest_Click;
+            // 
+            // RPG_Project
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(717, 643);
+            Controls.Add(btnTest);
+            Controls.Add(lblLevel);
+            Controls.Add(label5);
+            Controls.Add(lblExperience);
+            Controls.Add(lblGold);
+            Controls.Add(lblHitPoints);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "RPG_Project";
+            Text = "Game";
+            Load += RPG_Project_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label lblHitPoints;
+        private Label lblGold;
+        private Label lblExperience;
+        private Label label5;
+        private Label lblLevel;
+        private Button btnTest;
     }
 }
--- RPG_Project/RPG_Project.cs
+++ RPG_Project/RPG_Project.cs
@@ -6,5 +6,15 @@ namespace RPG_Project
         {
             InitializeComponent();
         }
+
+        private void RPG_Project_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblGold.Text = "123";
+        }
     }
 }
