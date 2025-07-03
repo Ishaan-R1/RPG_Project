f7c95c676e9db16ae2f9e4545a336bee9c8fb95d
Ishaan <ishaanrahim@gmail.com>
2025-07-03 20:33:29 +0100

    Refactoring program

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -10,8 +10,7 @@ namespace Engine
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public int MaximumHitPoints { get; set; }
-        public int CurrentHitPoints { get; set; }
+      
         public int MaximumDamage { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
